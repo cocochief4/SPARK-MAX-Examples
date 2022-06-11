d156161d9dae830673866c2285828740e66084e1
Cocochief <cocochief4@gmail.com>
2022-06-11 11:47:28 -0700

    IT works

--- Java/Tank Drive With CAN/src/main/java/frc/robot/Robot.java
+++ Java/Tank Drive With CAN/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -20,44 +22,49 @@ public class Robot extends TimedRobot {
   private Joystick m_rightStick;
   private static final int leftDeviceID = 1; 
   private static final int rightDeviceID = 2;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
+  private CANSparkMax m_leftMotorGroup;
+  private CANSparkMax m_rightMotorGroup;
+  private DigitalInput testInput = new DigitalInput(0);
 
   @Override
   public void robotInit() {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
-    // m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
-    // m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
-
     /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
+     * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+     * 
+     * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+     * first parameter
+     * 
+     * The motor type is passed as the second parameter. Motor type can either be:
+     *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+     *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+     * 
+     * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
+     * these parameters to match your setup
      */
-    // m_leftMotor.restoreFactoryDefaults();
-    // m_rightMotor.restoreFactoryDefaults();
+      m_leftMotorGroup = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
+      m_rightMotorGroup = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
 
-    // m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+      /*
+       * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+       * in the SPARK MAX to their factory default state. If no argument is passed, these
+       * parameters will not persist between power cycles
+       */
+      m_leftMotorGroup.restoreFactoryDefaults();
+      m_rightMotorGroup.restoreFactoryDefaults();
 
-    // m_leftStick = new Joystick(1);
-    // m_rightStick = new Joystick(5);
+      m_myRobot = new DifferentialDrive(m_leftMotorGroup, m_rightMotorGroup);
+
+      m_leftStick = new Joystick(1);
+      m_rightStick = new Joystick(5);
   }
   
-  @Override
   public void teleopInit() {
-    // m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
     System.out.println("hellow world");
+  }
+
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(.5, .5);
+    // System.out.println(testInput.get());
+    System.out.println(m_leftMotorGroup.get());
   }
 }
